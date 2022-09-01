5ee6d90c76cb1d2f1c28a84f78aaf1d438ff04a5
ChristopherEeles <christopher.eeles@outlook.com>
2022-09-01 20:12:05 +0000

    Add script to load best fit ASCAT objects; working on conversion to GRanges objects

--- scripts/4_selectOptimalGamma.R
+++ scripts/4_selectOptimalGamma.R
@@ -27,3 +27,12 @@ gamma_df <- rbindlist(
 )
 
 best_fits <- gamma_df[, .SD[which.max(GoF)], by="sample_name"]
+
+best_fit_files <- Map(
+    function(x, y)
+        grep(pattern=y, list.files(x, recursive=TRUE, full.names=TRUE), value=TRUE),
+    x=file.path(params$out_dir, best_fits$sample_name),
+    y=paste0(".*gamma", sprintf("%.2f", best_fits$gamma), "/.*RDS$")
+)
+
+best_fit_data <- BiocParallel::bplapply(best_fit_files, FUN=readRDS)
